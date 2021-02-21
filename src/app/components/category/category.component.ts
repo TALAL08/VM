a882b339f64df9a9fcd57ff9bb7126164ef144a1
talal Saleem <talal.salim150@gmail.com>
2021-02-21 23:38:56 +0500

    modify

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { CategoryService } from 'src/app/services/category.service';
 
@@ -14,55 +15,104 @@ import { CategoryService } from 'src/app/services/category.service';
     '../../../assets/vendors/css/vendor.bundle.base.css',
     '../../../assets/vendors/css/vendor.bundle.addons.css',
     '../../../assets/css/shared/style.css',
-    '../../../assets/css/demo_1/style.css'
-  ]
+    '../../../assets/css/demo_1/style.css',
+  ],
 })
 export class CategoryComponent implements OnInit {
   form!: FormGroup;
-  categoryId:string="";
+  categoryId: string = '';
+  categoryImage: { image: string; contentType: string } = {
+    image: '',
+    contentType: '',
+  };
   constructor(
     private route: ActivatedRoute,
-    private categoryService:CategoryService,
+    private sanitizer: DomSanitizer,
+    private categoryService: CategoryService,
     private fb: FormBuilder
   ) {
-
     this.form = this.fb.group({
-      name: ['', Validators.required]
-
+      name: ['', Validators.required],
+      image: ['', Validators.required],
+      contentType: ['', Validators.required]
     });
   }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((pram) => {
+      this.categoryId = pram.get('id') as string;
 
-    this.route.paramMap.subscribe(pram => {
-      this.categoryId = (pram.get('id') as string);
-
-      if(this.categoryId != null)
-      {
-        this.categoryService.get(this.categoryId).subscribe(res =>{
-
-          let categoryInDb = (res as any)
+      if (this.categoryId != null) {
+        this.categoryService.get(this.categoryId).subscribe((res) => {
+          let categoryInDb = res as any;
+          console.log(categoryInDb);
           this.form.get('name')?.setValue(categoryInDb.name);
+          this.form.get('image')?.setValue(categoryInDb.image);
+          this.form.get('contentType')?.setValue(categoryInDb.contentType);
+
+          this.categoryImage.image= categoryInDb.image;
+          this.categoryImage.contentType= categoryInDb.contentType;
         });
       }
-
     });
   }
 
-  submit(){
+  convertImage(event: any) {
+    const file = event.target.files[0];
+    console.log(file);
+    let form = this.form;
+    let categoryImage = this.categoryImage;
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+      const imageString = reader.result as string;
+      const contentType = imageString.slice(0, imageString.indexOf(',') + 1);
+      const image = imageString.slice(imageString.indexOf(',') + 1);
 
-    if(this.categoryId == null){
+      form.get('image')?.setValue(image);
+      form.get('contentType')?.setValue(contentType);
+      categoryImage.image = image;
+      categoryImage.contentType = contentType;
+      reader.onerror = function (error) {
+        console.log('Error: ', error);
+      };
+    };
+  }
 
-      this.categoryService.create(null,null,[this.form.get('name')?.value]).subscribe(res => {
+  getImage(image: any) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(image as string);
+  }
+
+  removeImage() {
+    this.form.get('image')?.reset;
+    this.form.get('contentType')?.reset;
+    this.categoryImage.image = '';
+    this.categoryImage.contentType = '';
+  }
+
+  submit() {
+    if (this.form.valid) {
+      const resource = this.form.value;
+
+      if (this.categoryId == null) {
+        this.createCategory(resource);
+      } else {
+        this.updateCategory(resource);
+      }
+    }
+  }
+
+  private updateCategory(resource: any) {
+    this.categoryService
+      .update(resource, null, [this.categoryId])
+      .subscribe((res) => {
         console.log(res);
       });
+  }
 
-    }
-    else
-    {
-      this.categoryService.update(null,null,[this.categoryId,this.form.get('name')?.value]).subscribe(res => {
-        console.log(res);
-      });
-    }
-}
+  private createCategory(resource: any) {
+    this.categoryService.create(resource).subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { CategoryService } from 'src/app/services/category.service';
 import { ProductService } from 'src/app/services/product.service';
@@ -23,9 +24,13 @@ export class ProductComponent implements OnInit {
   categories: any[] = [];
   productId: string = '';
   categoryId: string = '';
-
+  productImage: { image: string; contentType: string } = {
+    image: '',
+    contentType: '',
+  };
   constructor(
     private route: ActivatedRoute,
+    private sanitizer: DomSanitizer,
     private productService: ProductService,
     private categoryService: CategoryService,
     private fb: FormBuilder
@@ -34,12 +39,14 @@ export class ProductComponent implements OnInit {
       name: ['', Validators.required],
       categoryId: ['', Validators.required],
       isCategoryChange: [false],
+      image: ['', Validators.required],
+      contentType: ['', Validators.required],
     });
   }
 
   ngOnInit(): void {
     this.categoryService.getAll().subscribe((res) => {
-      this.categories = (res as any);
+      this.categories = res as any;
     });
 
     this.route.paramMap.subscribe((pram) => {
@@ -47,32 +54,79 @@ export class ProductComponent implements OnInit {
 
       if (this.productId != null) {
         this.productService.get(this.productId).subscribe((res) => {
-          let productInDb = (res as any);
+          let productInDb = res as any;
+
           this.form.get('name')?.setValue(productInDb.name);
           this.form.get('categoryId')?.setValue(productInDb.category.id);
+          this.form.get('image')?.setValue(productInDb.image);
+          this.form.get('contentType')?.setValue(productInDb.contentType);
+
+          this.productImage.image = productInDb.image;
+          this.productImage.contentType = productInDb.contentType;
           this.categoryId = productInDb.categoryId;
         });
       }
     });
   }
 
+  convertImage(event: any) {
+    const file = event.target.files[0];
+    let form = this.form;
+    let productImage = this.productImage;
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+      const imageString = reader.result as string;
+      const contentType = imageString.slice(0, imageString.indexOf(',') + 1);
+      const image = imageString.slice(imageString.indexOf(',') + 1);
+
+      form.get('image')?.setValue(image);
+      form.get('contentType')?.setValue(contentType);
+      productImage.image = image;
+      productImage.contentType = contentType;
+      reader.onerror = function (error) {
+        console.log('Error: ', error);
+      };
+    };
+  }
+
+  getImage(image: any) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(image as string);
+  }
+
+  removeImage() {
+    this.form.get('image')?.reset;
+    this.form.get('contentType')?.reset;
+    this.productImage.image = '';
+    this.productImage.contentType = '';
+  }
+
   submit() {
     if (this.form.valid) {
+      const resource = this.form.value;
       if (this.productId == null) {
-        this.productService.create(this.form.value).subscribe((res) => {
-          console.log(res);
-        });
+        this.createProduct(resource);
       } else {
-        let categoryId = this.form.get('categoryId')?.value;
-        if (categoryId != this.categoryId)
-          this.form.get('isCategoryChange')?.setValue(true);
-
-        this.productService
-          .update(this.form.value, null, [this.productId])
-          .subscribe((res) => {
-            console.log(res);
-          });
+        this.updateProduct(resource);
       }
     }
+  }
+
+  private createProduct(resource: any) {
+    this.productService.create(resource).subscribe((res) => {
+      console.log(res);
+    });
+  }
+
+  private updateProduct(resource: any) {
+    let categoryId = this.form.get('categoryId')?.value;
+    if (categoryId != this.categoryId)
+      this.form.get('isCategoryChange')?.setValue(true);
+
+    this.productService
+      .update(resource, null, [this.productId])
+      .subscribe((res) => {
+        console.log(res);
+      });
   }
 }
