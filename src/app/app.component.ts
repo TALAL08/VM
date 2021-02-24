bb746bbffd041a0312c0898424d4e393e5179dd6
talal Saleem <talal.salim150@gmail.com>
2021-02-24 21:30:17 +0500

    categoryProductDone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,15 @@ import { AuthService } from './services/auth.service';
     '../assets/vendors/css/vendor.bundle.base.css',
     '../assets/vendors/css/vendor.bundle.addons.css',
     '../assets/css/shared/style.css',
-    '../assets/css/demo_1/style.css'
+    '../assets/css/demo_1/style.css',
+    '../assets/lib/bootstrap/dist/css/bootstrap.min.css',
+    '../assets/lib/animate.css/animate.css',
+    '../assets/lib/components-font-awesome/css/font-awesome.min.css',
+    '../assets/lib/et-line-font/et-line-font.css',
+    '../assets/lib/magnific-popup/dist/magnific-popup.css',
+    '../assets/lib/simple-text-rotator/simpletextrotator.css',
+    '../assets/css/style.css',
+    '../assets/css/colors/default.css'
     ]
 })
 export class AppComponent {
--- src/app/components/category-products/category-products.component.ts
+++ src/app/components/category-products/category-products.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { CategoryProductService } from 'src/app/services/category-product.service';
 
 @Component({
   selector: 'app-category-products',
   templateUrl: './category-products.component.html',
-  styleUrls: ['./category-products.component.css']
+  styleUrls: [
+    './category-products.component.css',
+    '../../../assets/lib/bootstrap/dist/css/bootstrap.min.css',
+    '../../../assets/lib/animate.css/animate.css',
+    '../../../assets/lib/components-font-awesome/css/font-awesome.min.css',
+    '../../../assets/lib/et-line-font/et-line-font.css',
+    '../../../assets/lib/magnific-popup/dist/magnific-popup.css',
+    '../../../assets/lib/simple-text-rotator/simpletextrotator.css',
+    '../../../assets/css/style.css',
+    '../../../assets/css/colors/default.css',
+  ],
 })
 export class CategoryProductsComponent implements OnInit {
+  categoryProducts: any[] = [];
+  category: any;
+  constructor(
+    private route: ActivatedRoute,
+    private sanitizer: DomSanitizer,
+    private categoryProductService: CategoryProductService
+  ) {
+   }
 
-  constructor() { }
+   ngOnInit(): void {
 
-  ngOnInit(): void {
-  }
+    this.route.paramMap.subscribe((pram) => {
 
+      let categoryId = pram.get('categoryId') as string;
+      this.categoryProductService.get(categoryId).subscribe((res) => {
+        this.categoryProducts =(res as any);
+
+        this.category =this.categoryProducts[0].category
+      });
+    });
+
+
+   }
+
+
+   getImage(categoryImage: any) {
+
+
+       const image = categoryImage.contentType + categoryImage.image;
+       console.log(image);
+       return this.sanitizer.bypassSecurityTrustResourceUrl(image as string);
+
+   }
 }
