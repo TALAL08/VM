196c06071a1dd36ea55ddc4e1ae3650e676e21b7
talal Saleem <talal.salim150@gmail.com>
2021-04-08 19:51:39 +0500

    https

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -11,7 +11,7 @@ import { NotFound } from '../common/validator/not-found';
 })
 export class DataService {
     // private url:string="https://localhost:44329/";
-    private url:string="http://virtualmart-001-site1.etempurl.com/";
+    private url:string="https://virtualmart-001-site1.etempurl.com/";
     private httpOptions: any = {
 
         headers: new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': "Bearer " + (localStorage.getItem('token') as string) })
